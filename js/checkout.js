a9d21deb51a95cb7b9377f1b6212f03bbf6aecb7
Ilmira Dozhdikova <dozhdikovailmira@gmail.com>
2024-12-16 13:14:45 +0100

    Finalized

--- js/checkout.js
+++ js/checkout.js
@@ -1,28 +1,63 @@
 
 // Exercise 6
 function validate() {
-	var error = 0;
+
 	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
-
-	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
+	let fName = document.getElementById("fName");
+	let fEmail = document.getElementById("fEmail");
+	let fPassword = document.getElementById("fPassword");
+	let fAddress = document.getElementById("fAddress");
+	let fLastN = document.getElementById("fLastN");
+	let fPhone = document.getElementById("fPhone");
 	
-	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
-		error++;
-	}
+	let forms = document.querySelectorAll('.needs-validation');
+		forms.forEach(function (form) {
+  		form.addEventListener('submit', function (event) {
+			if (!form.checkValidity()) {
+				event.preventDefault();
+				event.stopPropagation();
+			}
+    form.classList.add('was-validated');
 
-	if(fEmail.value == ""){
-		error++;
-	}
-	 
-	if(error>0){
-		alert("Error");
-	}else{
-		alert("OK");
-	}
+		if (/\d/.test(fName.value)) {
+			fName.setCustomValidity('This field is required and must contain only letters, at least, 3 characters.')
+		} else {
+			fName.setCustomValidity('');
+		}
+		if (!fName.checkValidity()) {
+			event.preventDefault();
+			event.stopPropagation();
+		}
+		if (/\d/.test(fLastN.value)) {
+			fLastN.setCustomValidity('This field is required and must contain only letters, at least, 3 characters.');
+		} else {
+			fLastN.setCustomValidity('');
+		}
+		if (!fLastN.checkValidity()) {
+			event.preventDefault();
+			event.stopPropagation();
+		}
+		if (/[a-zA-Z]/.test(fPhone.value)) {
+			fPhone.setCustomValidity();
+		} else {
+			fPhone.setCustomValidity('');
+		}
+		if (!fPhone.checkValidity()) {
+			event.preventDefault();
+			event.stopPropagation();
+		}
+		if (/(?=.*[a-zA-Z])(?=.*\d)/.test(fPassword.value)) {
+			fPassword.setCustomValidity();
+		} else {
+			fPassword.setCustomValidity('');
+		}
+		if (!fPassword.checkValidity()) {
+			event.preventDefault();
+			event.stopPropagation();
+		}
 
+  }, false); 
+	
+});
+    
 }
